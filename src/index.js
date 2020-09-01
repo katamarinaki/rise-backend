c16f7119e49679a0537614d3d7c218555b93f67b
Alexander Belokon <ikatamarinaki@gmail.com>
2020-09-01 10:10:07 +0300

    add twitter streams support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import Koa from 'koa'
 import KoaRouter from '@koa/router'
 import websockify from 'koa-websocket'
-import { Twitter } from './twitter'
+import {
+  getPreparedTrendFromTweets,
+  openTwitterStream,
+  Twitter,
+} from './twitter'
 import { get } from 'lodash'
 
 const app = websockify(new Koa())
+const wsRouter = new KoaRouter()
 const router = new KoaRouter()
 
-// {"command": "getTweets", "args": {"q": "from:Katamarinaki", "count": "10"}}
-router.all('/twitter', async (ctx) => {
+// {"command": "getTweets", "args": {"q": "Belarus", "count": "5"}}
+wsRouter.all('/twitter', async (ctx) => {
   ctx.websocket.on('message', (message) => {
     try {
       const messageObj = JSON.parse(message)
@@ -21,10 +26,27 @@ router.all('/twitter', async (ctx) => {
         case 'getTweets':
           Twitter.get('search/tweets', {
             q: args.q,
-            count: parseInt(args.count),
-          }).then((d) => {
-            ctx.websocket.send(JSON.stringify(d.data))
+            count: 10,
+            include_entities: false,
+            include_rts: false,
+            include_user_entities: false,
           })
+            .then((d) => {
+              const tweets = get(d.data, 'statuses')
+              if (!tweets) {
+                throw 'No tweets found'
+              }
+              const trendingCause = getPreparedTrendFromTweets(tweets, args.q)
+              ctx.websocket.send(JSON.stringify(trendingCause))
+              openTwitterStream(ctx, args)
+            })
+            .catch((err) => {
+              throw err
+            })
+
+          break
+        case 'ping':
+          ctx.websocket.send(message)
           break
         default:
           throw 'Unknown command'
@@ -37,7 +59,13 @@ router.all('/twitter', async (ctx) => {
   })
 })
 
-app.ws.use(router.routes())
-app.ws.use(router.allowedMethods())
+router.get('/ping', async (ctx) => {
+  ctx.body = 'PONG'
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.ws.use(wsRouter.routes())
+app.ws.use(wsRouter.allowedMethods())
 
 app.listen(5000)
--- src/twitter/index.js
+++ src/twitter/index.js
@@ -1,5 +1,9 @@
 import Twit from 'twit'
 import 'dotenv/config'
+import { DateTime } from 'luxon'
+import { nanoid } from 'nanoid'
+import { get, sum } from 'lodash'
+import db from '../db'
 
 export const Twitter = new Twit({
   consumer_key: process.env.T_CONSUMER_KEY,
@@ -9,3 +13,100 @@ export const Twitter = new Twit({
   timeout_ms: 60 * 1000,
   // strictSSL: true, // optional - requires SSL certificates to be valid.
 })
+
+export const openTwitterStream = (ctx, args) => {
+  var stream = Twitter.stream('statuses/filter', { track: args.q })
+  stream.on('error', () => stream.stop())
+
+  stream.on('tweet', (tweet) => {
+    const trendingCause = getPreparedTrendFromTweets([tweet], args.q)
+    ctx.websocket.send(JSON.stringify(trendingCause))
+  })
+}
+
+const getPreparedTrendByKeyword = (keyword) => {
+  const rawTrend = db.get('trendingCauses').find({ keyword }).value()
+  if (!rawTrend) {
+    return null
+  }
+  const score = Math.floor(rawTrend.sumScore / rawTrend.count)
+  return {
+    id: rawTrend.id,
+    name: keyword,
+    value: score,
+    maxValue: score,
+    isSelected: false,
+    color: getTrendColor(score),
+  }
+}
+
+export const getPreparedTrendFromTweets = (tweets, keyword) => {
+  const scoreObjects = tweets.map((t) => getScoreFromTweet(t, keyword))
+  const sumScore = sum(scoreObjects)
+  const count = get(scoreObjects, 'length', 0)
+  const existingCause = db.get('trendingCauses').find({ keyword })
+  const existingCauseValue = existingCause.value()
+  if (!existingCauseValue) {
+    db.get('trendingCauses')
+      .push({
+        id: nanoid(),
+        keyword,
+        sumScore,
+        count,
+      })
+      .write()
+  } else {
+    existingCause
+      .assign({
+        sumScore: existingCauseValue.sumScore + sumScore,
+        count: existingCauseValue.count + count,
+      })
+      .write()
+  }
+  return getPreparedTrendByKeyword(keyword)
+}
+
+const getScoreFromTweet = (tweet) => {
+  const currDate = DateTime.local()
+  const tweetDate = DateTime.fromFormat(
+    tweet.created_at,
+    'EEE MMM dd HH:mm:ss ZZZ yyyy'
+  )
+  const likeScore = parseInt(tweet.favorite_count, 10) * 0.2
+  const rtScore = parseInt(tweet.retweet_count, 10) * 0.3
+  const timeScore =
+    currDate.minus({ seconds: tweetDate.toSeconds() }).toSeconds() * 0.5
+
+  let finalScore = Math.floor(likeScore + rtScore + timeScore)
+
+  if (finalScore < 0) {
+    return 0
+  }
+
+  return finalScore
+}
+
+const getTrendColor = (score) => {
+  switch (true) {
+    case score < 10:
+      return '#602BD0'
+    case score < 20:
+      return '#2353FF'
+    case score < 30:
+      return '#53A3ED'
+    case score < 40:
+      return '#32DAFF'
+    case score < 80:
+      return '#00FFC2'
+    case score < 100:
+      return '#5EFD81'
+    case score < 120:
+      return '#C8F34E'
+    case score < 160:
+      return '#E5E922'
+    case score < 200:
+      return '#FFCD4C'
+    default:
+      return '#FF4D4D'
+  }
+}
